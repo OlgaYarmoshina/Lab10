95be24eeea7df53e3cb98c51952f47da414d7738
LyadovOA <oleg29041998@gmail.com>
2024-05-20 19:54:48 +0400

    ver2

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import streamlit as st
 
 st.image('titanic.jpg')
-#st.header('Данные пассажиров Титаника')
-#st.write("Для просмотра данных о стоимости билетов , выберите пункт из списка ")
-#selected = st.selectbox('Выберите тип стоимости', ['Средняя цена', 'Максимальная цена', 'Минимальная цена'])
+st.header('Данные пассажиров Титаника')
+st.write("Для просмотра данных о стоимости билетов , выберите пункт из списка ")
+selected = st.selectbox('Выберите тип стоимости', ['Средняя цена', 'Максимальная цена', 'Минимальная цена'])
 
-#fig = plt.figure(figsize=(10, 5))
-#plt.bar = 1
-#plt.xlabel("Пол")
-#plt.ylabel("Цена")
-#st.pyplot(fig)
+fig = plt.figure(figsize=(10, 5))
+plt.bar = 1
+plt.xlabel("Пол")
+plt.ylabel("Цена")
+plt.title("Минимальная стоимость билетов")
+st.pyplot(fig)
