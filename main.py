1111567f941421d1b172397a58e3ddc83e71743c
LyadovOA <oleg29041998@gmail.com>
2024-05-20 21:19:26 +0400

    ver3

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import streamlit as st
 from var10 import do_var10
 
 st.image('titanic.jpg')
-choice = st.selectbox('Вариант номер:',  ['вариант 10','вариант 12','вариант 13'])
+choice = st.selectbox('Вариант номер:',  ['вариант 10','вариант 1','вариант 18'])
 
 if choice == 'Вариант 10':
     do_var10
-elif choice == 'Вариант 12':
+elif choice == 'Вариант 1':
     pass
 
