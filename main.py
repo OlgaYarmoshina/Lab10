f393e21e8232d6412847f95725ca1249ba11aad5
LyadovOA <oleg29041998@gmail.com>
2024-05-20 18:30:07 +0400

    init commit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import streamlit as st
 
-#st.image('titanic.jpg')
+st.image('titanic.jpg')
 st.header('Данные пассажиров Титаника')
 st.write("Для просмотра данных о стоимости билетов , выберите пункт из списка ")
 selected = st.selectbox('Выберите тип стоимости', ['Средняя цена', 'Максимальная цена', 'Минимальная цена'])
