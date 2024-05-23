0fa444a73e76ca3ab572563d79e530b498ca2a54
LyadovOA <168270329+LyadovOA@users.noreply.github.com>
2024-05-23 10:17:59 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 import streamlit as st
+
+st.header('Практическое занятие №11')
+st.image('Titanic.jpg')
+st.header('Работа группы 2023-ФГиИБ-ПИ-2см: Подгруппа №2 ')
+
+choice = st.selectbox('Вариант номер:',  ['вариант 10','вариант 5','вариант 8','вариант 1', 'вариант 15', 'вариант 18'])
+
+#from var10 import do_var1
+#from var10 import do_var5
+from var8 import do_var8
 from var10 import do_var10
+#from var10 import do_var15
+#from var10 import do_var18
 
-st.image('titanic.jpg')
-choice = st.selectbox('Вариант номер:',  ['вариант 10','вариант 1','вариант 18','вариант 8'])
-
-if choice == 'Вариант 10':
-    do_var10
-elif choice == 'Вариант 1':
-    pass
+if choice == 'вариант 10':
+    do_var10()
+#elif choice == 'вариант 5':
+    #do_var5()
+elif choice == 'вариант 8':
+    do_var8()
+#elif choice == 'вариант 1':
+    #do_var1()
+#elif choice == 'вариант 15':
+   # do_var15()
+#elif choice == 'вариант 18':
+    #do_var18()
 
